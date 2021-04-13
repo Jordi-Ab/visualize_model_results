7debee70d6182ccd197bc4db05ba0a12559ed3c8
Jordi-Ab <jordi_a3@hotmail.com>
2021-04-13 16:44:38 -0500

    setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+from wrapper_scripts import FilePicker
+
+setup(
+    name="visualize_model_results",
+    version="0.1",
+    description="Most useful plots to assess ML models performance",
+    url="https://github.com/Jordi-Ab/visualize_model_results",
+    author="Giordi Azonos Beverido",
+    author_email="jordi_a3@hotmail.com",
+    packages=find_packages(
+        include=[
+            "features_pipeline", 
+            "features_pipeline.*"
+        ]
+    ),
+    install_requires=[
+        "pandas",
+        "numpy",
+        "matplotlib",
+        "sklearn"
+    ],
+    zip_safe=False,
+)
